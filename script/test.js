0766895f440954f07e15d032728c2fd4364eab56
Sopico1x <sopico59@gmail.com>
2024-11-19 15:29:10 +0100

    debut js

---
+++ script/test.js
@@ -0,0 +1,11 @@
+//console.log("hello world!");
+
+//let userName = "Sopico le Goat"; //string
+//let age = 18; //int
+//let size = 1.75 //float
+//let isCool = true; //bool
+
+let a = 15
+let b = 35
+
+console.log(a+b, a/b, a-b, a*b, a%b);
